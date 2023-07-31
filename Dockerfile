beafdb8dd6737ad896eb64cd48e16814a8afb9bd
Kaushik Vinjamuri <kaushik.vinjamuri@syntasa.com>
2023-07-30 23:17:55 -0400

    fix haproxy, ingress, and sa policy

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM gcr.io/cloud-marketplace-tools/k8s/deployer_helm/onbuild
 
 # Add timeouts so we don't have to wait a full 5 minutes for the deployer to timeout
-ENV WAIT_FOR_READY_TIMEOUT 100
-ENV TESTER_TIMEOUT 100
+ENV WAIT_FOR_READY_TIMEOUT 120
+ENV TESTER_TIMEOUT 120
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 export PROJECT="$(gcloud config get-value project | tr ':' '/')"
 export REGISTRY=gcr.io/syntasa-public
+export DEPLOYER_TAG_SEM=7.1
 export DEPLOYER_TAG=7.1.2
 export TAG=latest
 export APP_NAME=syntasa-behaviorial-sentiment-analytics
+
+
+docker build \
+--build-arg REGISTRY=$REGISTRY \
+--build-arg APP_NAME=$APP_NAME \
+--build-arg TAG=$TAG \
+--build-arg PROJECT=$PROJECT \
+--tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM .
 
 docker build \
 --build-arg REGISTRY=$REGISTRY \
@@ -12,4 +21,5 @@ docker build \
 --build-arg PROJECT=$PROJECT \
 --tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG .
 
+docker push $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM
 docker push $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG
