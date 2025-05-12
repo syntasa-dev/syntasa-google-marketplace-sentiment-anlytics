8735eca2a01db960b6b11b15dcb92c9953b30743
nikhil.kumar <nikhil.kumar@syntasa.com>
2025-05-13 03:26:40 +0530

    fixed  deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,40 @@ export DEPLOYER_TAG=8.2.1
 export TAG=8.2.1
 export APP_NAME=syntasa-behaviorial-sentiment-analytics
 
-docker build \
---build-arg REGISTRY=$REGISTRY \
---build-arg APP_NAME=$APP_NAME \
---build-arg TAG=$TAG \
---build-arg PROJECT=$PROJECT \
---tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM .
+docker build --platform linux/amd64 \
+  --build-arg REGISTRY=$REGISTRY \
+  --build-arg APP_NAME=$APP_NAME \
+  --build-arg TAG=$TAG \
+  --build-arg PROJECT=$PROJECT \
+  --tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM .
 
-docker build \
---build-arg REGISTRY=$REGISTRY \
---build-arg APP_NAME=$APP_NAME \
---build-arg TAG=$TAG \
---build-arg PROJECT=$PROJECT \
---tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG .
+docker build --platform linux/amd64 \
+  --build-arg REGISTRY=$REGISTRY \
+  --build-arg APP_NAME=$APP_NAME \
+  --build-arg TAG=$TAG \
+  --build-arg PROJECT=$PROJECT \
+  --tag $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG .
 
+docker build --platform linux/amd64 \
+  --build-arg REGISTRY=$REGISTRY \
+  --build-arg APP_NAME=$APP_NAME \
+  --build-arg TAG=$TAG \
+  --build-arg PROJECT=$PROJECT \
+  --tag $REGISTRY/$APP_NAME:$DEPLOYER_TAG_SEM .
+
+docker build --platform linux/amd64 \
+  --build-arg REGISTRY=$REGISTRY \
+  --build-arg APP_NAME=$APP_NAME \
+  --build-arg TAG=$TAG \
+  --build-arg PROJECT=$PROJECT \
+  --tag $REGISTRY/$APP_NAME:$DEPLOYER_TAG .
+
+# Push the built images
 docker push $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM
 docker push $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG
+
+docker push $REGISTRY/$APP_NAME:$DEPLOYER_TAG_SEM
+docker push $REGISTRY/$APP_NAME:$DEPLOYER_TAG
+
+
+docker manifest inspect $REGISTRY/$APP_NAME/deployer:$DEPLOYER_TAG_SEM
